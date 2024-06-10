549f69037103bad9c09ca33e823c4924f63b655b
lupeMorales <morales.lupe@gmail.com>
2024-06-10 16:27:55 +0200

    add login function y logout funtion in Auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,4 +35,28 @@ class AuthController extends Controller
 
         return response()->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer'], 201);
     }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid login details'], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Hi ' . $user->name,
+            'token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'You have successfully logged out and the token was successfully deleted']);
+    }
 };
