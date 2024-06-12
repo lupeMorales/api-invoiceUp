9750a8b25a8a2ba37bde3cbdbc30588c04496311
lupeMorales <morales.lupe@gmail.com>
2024-06-12 19:23:28 +0200

    solve register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 
-//use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -12,12 +12,15 @@ use Illuminate\Support\Facades\Validator;
 
 
 class AuthController extends Controller
+
+
 {
+
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
-            'email' => 'requires|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8'
         ]);
         if ($validator->fails()) {
@@ -33,10 +36,10 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer'], 201);
+        return response()->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer', 'message' => 'Congrats!'], 201);
     }
 
-    public function login(Request $request)
+    /*  public function login(Request $request)
     {
         if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json(['message' => 'Invalid login details'], 401);
@@ -52,6 +55,71 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
         ]);
     }
+ */
+    /*   public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        try {
+            $user = User::where('email', $request->email)->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Hi ' . $user->name,
+                'token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+    }
+ */
+    /*   public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Hi ' . $user->name,
+                'token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Invalid login details'
+        ], 401);
+    } */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Hi ' . $user->name,
+                'token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Invalid login details'
+        ], 401);
+    }
+
+
 
     public function logout(Request $request)
     {
